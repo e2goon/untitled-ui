8118eea2290bc24b1345267fb199d1090df2637b
wmlee <e2goon@gmail.com>
2020-01-20 02:42:47 +0900

    update: Grid > Col 컴포넌트 prop 추가

--- src/components/Grid/Col.js
+++ src/components/Grid/Col.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import styles from "./Grid.module.scss";
 
-export default function Col({ children, auto }) {
+export default function Col({ children, style, auto }) {
   const cls = classnames({
     [styles.col]: true,
     [styles.auto]: auto
   })
-  return <div className={cls}>{children}</div>;
+  return <div className={cls} style={style}>{children}</div>;
 }
 
 Col.propTypes = {
+  style: PropTypes.object,
   auto: PropTypes.bool
 }
