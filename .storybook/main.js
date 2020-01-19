892c2610854dbfcffd75846c8e20f9d76f1a039a
wmlee <e2goon@gmail.com>
2020-01-20 02:41:39 +0900

    add: 스토리북 CRA 프리셋 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ["../src/components/**/*.stories.(js|mdx)"],
   addons: [
     '@storybook/addon-docs',
-    '@storybook/addon-storysource'
+    '@storybook/addon-storysource',
+    '@storybook/preset-create-react-app'
   ]
 };
