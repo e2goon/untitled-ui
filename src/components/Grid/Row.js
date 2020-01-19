06668d317056f40e079ef88e2a78047675e527c8
wmlee <e2goon@gmail.com>
2020-01-20 03:44:58 +0900

    add: Row 컴포넌트 > 설명 추가

--- src/components/Grid/Row.js
+++ src/components/Grid/Row.js
@@ -1,6 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from './Grid.module.scss';
 
 export default function Row({ children }) {
   return <div className={styles.row}>{children}</div>;
+}
+
+Row.propTypes = {
+  /** `<Col />` 컴포넌트들을 사용할 수 있습니다. */
+  children: PropTypes.node.isRequired
 }
