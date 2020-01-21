2750b4c68bbdfd542c0ffd3a7d08430e4023c134
wmlee <e2goon@gmail.com>
2020-01-21 23:35:21 +0900

    update: Select 스토리북 추가

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -3,6 +3,12 @@ import PropTypes from "prop-types";
 import classnames from "classnames";
 import styles from "./Select.module.scss";
 
+/**
+ * `<Select />` 컴포넌트는 `<option>` 태그를 사용하여 네이티브한 기능을 그대로 사용할 수 있습니다. 인라인 스타일이나 클래스명을 중첩하여 폭넓은 디자인 커스터마이징을 할 수 있습니다.
+ * 
+ * 추후에 배열을 받아서 `<option>` 태그들을 리스트 형식으로 표시할 수 있는 기능을 계획하고 있습니다.
+ */
+
 export default function Select({ children, text, sm, className, style, block }) {
   const [val, setVal] = useState();
   const [txt, setTxt] = useState(text);
@@ -27,7 +33,8 @@ export default function Select({ children, text, sm, className, style, block }) 
 }
 
 Select.propTypes = {
-  children: PropTypes.node,
+  /** 선택할 수 있도록 `<option>` 태그를 사용해야 합니다. */
+  children: PropTypes.node.isRequired,
   className: PropTypes.string,
   text: PropTypes.string,
   sm: PropTypes.bool,
