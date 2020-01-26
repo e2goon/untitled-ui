832d66190e9193ce42f1d0d299a97e46a26e7a4a
wmlee <e2goon@gmail.com>
2020-01-27 02:24:27 +0900

    update: 스토리북 액션 애드온 설정 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ["../src/**/**/*.stories.(js|mdx)"],
   addons: [
+    '@storybook/addon-actions/register',
     '@storybook/addon-docs',
     '@storybook/addon-storysource',
     '@storybook/preset-create-react-app'
